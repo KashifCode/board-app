1d0d6a208410a5487e822d35e459b4512de18967
waleed-bee <waleed@brightbee.org>
2026-07-15 06:51:32 -0400

    reduce pen tool size

--- app/board/[boardId]/_components/path.tsx
+++ app/board/[boardId]/_components/path.tsx
@@ -54,7 +54,7 @@ export const Path = ({
                 onPointerEnter={onPointerEnter}
                 d={getSvgPathFromStroke(
                     getStroke(points, {
-                        size: 16,
+                        size: 4,
                         thinning: 0.5,
                         smoothing: 0.5,
                         streamline: 0.5,
